8f850dc9d21aeced47947d89e3c6a7892edf96ea
Zedd <zedd.0202@kakaocorp.com>
2019-04-08 14:05:52 +0900

    first commit

---
+++ Haptic Feedback/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Haptic Feedback
+//
+//  Created by Zedd on 06/04/2019.
+//  Copyright © 2019 Zedd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+class SomeClass {}
+protocol Zedd: SomeClass { }
