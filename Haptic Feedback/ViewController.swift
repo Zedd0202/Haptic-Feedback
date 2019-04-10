8725180bf96b38b2ecb0b92aeda99c7567683a89
Zedd <zedd.0202@kakaocorp.com>
2019-04-10 13:29:34 +0900

    add notification feedback

--- Haptic Feedback/ViewController.swift
+++ Haptic Feedback/ViewController.swift
@@ -10,12 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var feedbackGenerator: UINotificationFeedbackGenerator?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.setupGenerator()
     }
-
-
+    
+    private func setupGenerator() {
+        self.feedbackGenerator = UINotificationFeedbackGenerator()
+        self.feedbackGenerator?.prepare()
+    }
+    
+    @IBAction func success(_ sender: Any) {
+        self.feedbackGenerator?.notificationOccurred(.success)
+    }
+    
+    @IBAction func warning(_ sender: Any) {
+        self.feedbackGenerator?.notificationOccurred(.warning)
+    }
+    
+    @IBAction func error(_ sender: Any) {
+        self.feedbackGenerator?.notificationOccurred(.error)
+    }
 }
-class SomeClass {}
-protocol Zedd: SomeClass { }
+
+public class SomePublicClass {                   // explicitly public class
+    public var somePublicProperty = 0            // explicitly public class member
+    var someInternalProperty = 0                 // implicitly internal class member
+    fileprivate func someFilePrivateMethod() {}  // explicitly file-private class member
+    private func somePrivateMethod() {}          // explicitly private class member
+}
+
+public extension SomePublicClass {
+    public func somePublic() { }
+}
